aeadef520b55fd4fadc5e2aa6ed8990936970510
이예원 <62163559+Eyekrw@users.noreply.github.com>
2023-09-28 17:31:14 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (11.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if (n <= 2) return n;
+
+        vector<int> cache(n+1);
+        cache[1] = 1;
+        cache[2] = 2;
+        for(int i=3;i<=n;i++)
+        {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n];
+    }
+};
